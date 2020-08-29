7ff0e96e157d3ec9ca5bdd68d11473b3f08f3f3a
adcastiblanco <angelcastiblanco96@gmail.com>
2020-08-29 18:24:12 -0500

    add breakpoints.js for responsive design

--- src/components/Layout/Footer/styles.jsx
+++ src/components/Layout/Footer/styles.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
   background-color: var(--primary-color);
+  border-radius: 10px 10px 0 0;
 `;
 
 export const FooterText = styled.h3`
@@ -9,12 +10,16 @@ export const FooterText = styled.h3`
   margin: auto;
   color: white;
 `;
+export const ContainerFlex = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
 
 export const ContainerList = styled.ul`
   list-style: none;
-  color: white;
+  padding: 0;
 `;
 
-export const ContainerFlex = styled.div`
-  display: flex;
+export const ContainerColumn = styled.div`
+  color: white;
 `;
