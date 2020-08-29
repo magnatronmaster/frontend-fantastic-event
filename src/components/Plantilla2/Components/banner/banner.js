be261537a7f472573ae1d230dde717fdc9d3b98a
Nelson Alayon <neanmi@hotmail.com>
2020-08-29 08:55:45 -0500

    banner parametrizado

--- src/components/Plantilla2/Components/banner/banner.js
+++ src/components/Plantilla2/Components/banner/banner.js
@@ -1,17 +1,21 @@
 import React from "react"
 import {Baner, MainEvent} from "./styles"
 
+const NAME_EVENT = "Nombre del evento"
+const DATE_EVENT = "15 de ocubre de 2020"
 
-export const Banner = () => {
+
+export const Banner = ({nameEvent, dateEvent}) => {
     return (
         <Baner>
             <MainEvent>
              <p>Prepárate para<br/> tu próximo evento</p>
-                <h1>Nombre del Evento</h1>
-                <p>15 de ocubre de 2020</p>
+                <h1>{nameEvent || NAME_EVENT}</h1>
+                <p>{dateEvent || DATE_EVENT}</p>
             </MainEvent>
         </Baner>
     )
 
 
 }
+
