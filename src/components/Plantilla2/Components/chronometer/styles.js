62d22e1c7bd45024b004a17031c1c96ea55b576f
Nelson Alayon <neanmi@hotmail.com>
2020-08-29 09:59:13 -0500

    contador arreglado

--- src/components/Plantilla2/Components/chronometer/styles.js
+++ src/components/Plantilla2/Components/chronometer/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 export const CountdownWrapper = styled.div`
 display:flex;
 margin:20px;
-transform:scale(1.5);
-margin-left:300px;
+@media only screen and(max-width = 425px) {
+    margin:auto;
+}
+
 `
