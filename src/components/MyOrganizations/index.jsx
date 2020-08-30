17da05c603f5ceffc01d74eda086b3a96e63ec0a
adcastiblanco <angelcastiblanco96@gmail.com>
2020-08-30 16:02:12 -0500

    Create organization complete and redirect when the user isn't logged

--- src/components/MyOrganizations/index.jsx
+++ src/components/MyOrganizations/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import useInitialState from 'hooks/useInitialState';
 
 import { ContainerCards, Button } from 'assets/GlobalStyles';
 import { CardButton } from 'containers/CardButton';
