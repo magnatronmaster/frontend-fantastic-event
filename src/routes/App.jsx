530c45fcb0502dfe96a2da33eb7822179e8ce01b
adcastiblanco <angelcastiblanco96@gmail.com>
2020-08-30 17:08:55 -0500

    fix bugs

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -18,38 +18,42 @@ import { Layout } from '../components/Layout';
 import { Login } from '../components/Auth/Login';
 import { Register } from '../components/Auth/Register';
 
-
-function SecureRoutes({ component, path, ...restElements }) {
+const App = () => {
   const token = localStorage.token_user;
   const username = localStorage.id_user;
-  if (token && username) {
-    return <Route path={path} component={component} {...restElements} />;
-  } else {
-    return <Redirect to="/login" {...restElements} />;
+  function SecureRoutes({ component, path, ...restElements }) {
+    if (token && username) {
+      return <Route path={path} component={component} {...restElements} />;
+    } else {
+      return <Redirect to="/login" {...restElements} />;
+    }
   }
-}
-
-const App = () => (
-  <BrowserRouter>
-    <GlobalStyles />
-    <Switch>
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/" component={Home} />
-      <Layout>
-        <SecureRoutes exact path="/dashboard" component={MainDashboard} />
-        <SecureRoutes exact path="/adminDashboard" component={AdminDashboard} />
-        <SecureRoutes exact path="/editEvent" component={EditEvent} />
-        <SecureRoutes exact path="/sponsors" component={Sponsors} />
-        <SecureRoutes exact path="/conferences" component={Conferences} />
-        <SecureRoutes exact path="/lecturers" component={Lecturers} />
-        <SecureRoutes exact path="/addLecturer" component={AddLecturer} />
-        <SecureRoutes exact path="/addConference" component={AddConference} />
-        <SecureRoutes exact path="/addSponsor/:type" component={AddSponsor} />
-      </Layout>
-    </Switch>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Switch>
+        {!token && <Route exact path="/login" component={Login} />}
+        {!token && <Route exact path="/register" component={Register} />}
+        {!token && <Route exact path="/" component={Home} />}
+        <Layout>
+          <SecureRoutes exact path="/dashboard" component={MainDashboard} />
+          <SecureRoutes
+            exact
+            path="/adminDashboard"
+            component={AdminDashboard}
+          />
+          <SecureRoutes exact path="/editEvent" component={EditEvent} />
+          <SecureRoutes exact path="/sponsors" component={Sponsors} />
+          <SecureRoutes exact path="/conferences" component={Conferences} />
+          <SecureRoutes exact path="/lecturers" component={Lecturers} />
+          <SecureRoutes exact path="/addLecturer" component={AddLecturer} />
+          <SecureRoutes exact path="/addConference" component={AddConference} />
+          <SecureRoutes exact path="/addSponsor/:type" component={AddSponsor} />
+        </Layout>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
 <AddSponsor />;
